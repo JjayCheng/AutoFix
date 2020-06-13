0f7a2d4bbdb72429065caa89e54bec8fd3e1da2a
JjayCheng <1214934840@qq.com>
2020-06-13 10:08:03 +0800

    first commit

---
+++ autofix/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace autofix
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(0, 439);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(476, 10);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("宋体", 12F);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "住院管理",
+            "住院工作站",
+            "门诊工作站",
+            "门诊收费",
+            "病案管理",
+            "后勤管理",
+            "西药库房",
+            "西药药房",
+            "中药库房",
+            "中药药房"});
+            this.comboBox1.Location = new System.Drawing.Point(7, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(208, 24);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = "--选择需要修复的模块--";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F);
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(7, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "His系统修复";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 12F);
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(7, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "默认打印机设置";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("宋体", 12F);
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(10, 46);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(260, 24);
+            this.comboBox2.TabIndex = 4;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(133, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "默认打印机：";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.Font = new System.Drawing.Font("宋体", 12F);
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "住院管理",
+            "住院工作站",
+            "门诊工作站",
+            "门诊收费",
+            "病案管理",
+            "后勤管理",
+            "西药库房",
+            "西药药房",
+            "中药库房",
+            "中药药房",
+            "后勤库房"});
+            this.comboBox3.Location = new System.Drawing.Point(254, 41);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(202, 24);
+            this.comboBox3.TabIndex = 6;
+            this.comboBox3.Text = "--选择需要安装的模块--";
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 12F);
+            this.label4.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label4.Location = new System.Drawing.Point(251, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "His系统安装";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 421);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "本机ip";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(1, 3);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(475, 415);
+            this.tabControl1.TabIndex = 9;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.comboBox1);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.comboBox3);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(467, 348);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "His系统相关";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.comboBox2);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(467, 348);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "打印机相关";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(467, 389);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "网络相关";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "自助修复工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+    }
+}
+
---
+++ autofix/Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using IWshRuntimeLibrary;
+using File = System.IO.File;
+using System.Drawing.Printing;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Net.NetworkInformation;
+
+namespace autofix
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PrinterOperate.GetPrinters(this.label3, this.comboBox2);
+            this.label5.Text = "本机IP：" + NetworkOperate.GetLocalIP();
+            string strServer = @"\\192.168.1.3";
+            string strUserName = "administrator";
+            string strUserPD = "ybsnyyzq";
+            System.Diagnostics.Process.Start("net.exe", "use   \\\\" + strServer + "     /user:\"" + strUserName + "\"   \"" + strUserPD + "\"");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (MessageBox.Show( "选择为：" + this.comboBox1.Text, "确认选择正确？", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                string destPath = FilesOperate.GetPathInLnkFile(@"C:\Users\Administrator\Desktop\"+ this.comboBox1.Text + ".lnk");
+                BeginWork();
+                FilesOperate.MoveFolder(@"\\192.168.1.3\his90程序\"+ this.comboBox1.Text, destPath, this.progressBar1);
+                OverWork("修复完成！");
+            }
+            
+        }
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string printer = this.comboBox2.SelectedItem.ToString();
+            if (this.comboBox2.SelectedItem != null) //判断是否有选中值
+            {
+
+                if (PrinterOperate.SetDefaultPrinter(printer)) //设置默认打印机
+                {
+                    this.label3.Text = "默认打印机:" + printer;
+                    MessageBox.Show(printer + "设置为默认打印机成功！");
+                }
+                else
+                {
+                    MessageBox.Show(printer + "设置为默认打印机失败！");
+                }
+            }
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedText = this.comboBox3.Text;
+            string[] exeMap = {"zygl","zyys","ysgl","mzgl","bagl","hqgl","kfgl","yfgl","kfgl","yfgl","zwkf"};
+
+            if (MessageBox.Show("选择为：" + selectedText, "确认选择正确？", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                string destPath = @"D:\His程序\" + selectedText;
+                BeginWork();
+                FilesOperate.MoveFolder(@"\\192.168.1.3\his90程序\" + selectedText, destPath, this.progressBar1);
+                FilesOperate.CreateDesktopShortcut(selectedText, destPath + "\\" + exeMap[this.comboBox3.SelectedIndex] + ".exe");
+                OverWork("安装完成！");
+            }
+        }
+        private void BeginWork()
+        {
+            this.progressBar1.Value = 0;
+            this.Text = "开始修复.....";
+        }
+        private void OverWork(string message)
+        {
+            this.progressBar1.Value = 100;
+            MessageBox.Show(message);
+            this.progressBar1.Value = 0;
+            this.Text = "自助修复工具";
+        }
+    }
+
+}
+
+
+
+
+
+
+namespace autofix
+{
+    public class FilesOperate
+    {
+        public static string GetPathInLnkFile(string path)
+        {
+            if (File.Exists(path))
+            {
+                WshShell shell = new WshShell();
+                IWshShortcut lnkFile = (IWshShortcut)shell.CreateShortcut(path);
+                //快捷方式文件指向的路径.Text = 当前快捷方式文件IWshShortcut类.TargetPath;
+                //快捷方式文件指向的目标目录.Text = 当前快捷方式文件IWshShortcut类.WorkingDirectory;
+                return lnkFile.WorkingDirectory;
+            }
+            else
+            {
+                return "";
+            }
+        }
+        /// <summary>
+        /// 移动文件夹中的所有文件夹与文件到另一个文件夹 //转载请注明来自 http://www.uzhanbao.com
+        /// </summary>
+        /// <param name="sourcePath">源文件夹</param>
+        /// <param name="destPath">目标文件夹</param>
+        public static void MoveFolder(string sourcePath, string destPath, ProgressBar progressBar)
+        {
+            if (Directory.Exists(sourcePath))
+            {
+                if (!Directory.Exists(destPath))
+                {
+                    //目标目录不存在则创建
+                    try
+                    {
+                        Directory.CreateDirectory(destPath);
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception("创建目标目录失败：" + ex.Message);
+                    }
+                }
+                //获得源文件下所有文件
+                List<string> files = new List<string>(Directory.GetFiles(sourcePath));
+                files.ForEach(c =>
+                {
+                    string destFile = Path.Combine(new string[] { destPath, Path.GetFileName(c) });
+                    //覆盖模式
+                    if (File.Exists(destFile))
+                    {
+                        File.Delete(destFile);
+                    }
+                    if(progressBar.Value <= 90)
+                    {
+                        progressBar.Value += 10;
+                    }
+                    File.Copy(c, destFile);
+                });
+                //获得源文件下所有目录文件
+                List<string> folders = new List<string>(Directory.GetDirectories(sourcePath));
+
+                folders.ForEach(c =>
+                {
+                    string destDir = Path.Combine(new string[] { destPath, Path.GetFileName(c) });
+                    MoveFolder(c, destDir, progressBar);
+                });
+            }
+            else
+            {
+                throw new DirectoryNotFoundException("源目录不存在！");
+            }
+        }
+        /// <summary>
+        /// 创建桌面快捷方式
+        /// </summary>
+        /// <param name="FileName">文件的名称</param>
+        /// <param name="exePath">EXE的路径</param>
+        /// <returns>成功或失败</returns>
+        public static bool CreateDesktopShortcut(string FileName, string exePath)
+        {
+            try
+            {
+                string deskTop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\";
+                if (File.Exists(deskTop + FileName + ".lnk"))  //
+                {
+                    File.Delete(deskTop + FileName + ".lnk");//删除原来的桌面快捷键方式
+                }
+                WshShell shell = new WshShell();
+
+                //快捷键方式创建的位置、名称
+                IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(deskTop + FileName + ".lnk");
+                shortcut.TargetPath = exePath; //目标文件                         
+                DirectoryInfo pathInfo = new DirectoryInfo(exePath);
+                string WorkingDirectory = pathInfo.Parent.FullName;  //该属性指定应用程序的工作目录，当用户没有指定一个具体的目录时，快捷方式的目标应用程序将使用该属性所指定的目录来装载或保存文件。
+                shortcut.WorkingDirectory = WorkingDirectory;
+                shortcut.WindowStyle = 1; 
+                shortcut.Description = FileName; //描述
+                shortcut.Save(); //必须调用保存快捷才成创建成功
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+    public class PrinterOperate
+    {
+        public static void GetPrinters (Label label, ComboBox combox)
+        {
+            PrintDocument print = new PrintDocument();
+            string sDefault = print.PrinterSettings.PrinterName;//默认打印机名
+            label.Text = "默认打印机:" + sDefault;
+            combox.Text = sDefault;
+            foreach (string sPrint in PrinterSettings.InstalledPrinters)//获取所有打印机名称
+            {
+                combox.Items.Add(sPrint);
+            }
+        }
+
+        [DllImport("winspool.drv")]
+        public static extern bool SetDefaultPrinter(String Name); //调用win api将指定名称的打印机设置为默认打印机
+    }
+    public class NetworkOperate
+    {
+        public static string GetLocalIP()
+        {
+            IPAddress localIp = null;
+            try
+            {
+                IPAddress[] ipArray;
+                ipArray = Dns.GetHostAddresses(Dns.GetHostName());
+                localIp = ipArray.First(ip => ip.AddressFamily == AddressFamily.InterNetwork);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.StackTrace + "\r\n" + ex.Message, "错误", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+            }
+            if (localIp == null)
+            {
+                localIp = IPAddress.Parse("127.0.0.1");
+            }
+            return localIp.ToString();
+        }
+        /// <summary>
+        /// Ping命令检测网络是否畅通
+        /// </summary>
+        /// <param name="urls">URL数据</param>
+        /// <param name="errorCount">ping时连接失败个数</param>
+        /// <returns></returns>
+        public static bool Ping(string url)
+        {
+            bool isconn = true;
+            Ping ping = new Ping();
+            try
+            {
+                PingReply pr;
+                pr = ping.Send(url);
+                if (pr.Status != IPStatus.Success)
+                {
+                    isconn = false;
+                }
+            }
+            catch
+            {
+                isconn = false;
+            }
+            //if (errorCount > 0 && errorCount < 3)
+            //  isconn = true;
+            return isconn;
+        }
+    }
+}
